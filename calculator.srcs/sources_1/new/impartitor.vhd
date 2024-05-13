c6ffb2e4fe6b6db2a45e3c8e5d6e3afa0f496bb1
Iulika2901 <84437252+Iulika2901@users.noreply.github.com>
2024-05-13 10:49:00 +0300

    Update impartitor.vhd

--- calculator.srcs/sources_1/new/impartitor.vhd
+++ calculator.srcs/sources_1/new/impartitor.vhd
@@ -37,13 +37,23 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 entity impartitor is
        Port ( a : in STD_LOGIC_VECTOR (7 downto 0);
         b : in STD_LOGIC_VECTOR(7 downto 0);
-        s : in STD_LOGIC_VECTOR (7 downto 0);
+        s : out STD_LOGIC_VECTOR (7 downto 0);
         bin : in STD_LOGIC;
         bout : out STD_LOGIC);
  
 end impartitor;
  
 architecture Behavioral of impartitor is
+ 
+ component scazator_complet is
+
+ Port ( a : in STD_LOGIC_vector (7 downto 0);
+         b : in STD_LOGIC_vector (7 downto 0);
+         bout : out STD_LOGIC;
+         bin : in STD_LOGIC;
+         s : out STD_LOGIC_vector (7 downto 0));
+end component;
+
  
   component scazator is
     Port ( a : in STD_LOGIC;
@@ -60,53 +70,41 @@ architecture Behavioral of impartitor is
  
   signal borrow : std_logic_vector(7 downto 0);
   signal cp_i: std_logic_vector(3 downto 0);
-  signal cp_o: std_logic_vector(3 downto 0);
+  signal cp_o: std_logic_vector(6 downto 0);
  
 begin
   borrow(0) <= bin;
-  bout <=  borrow(7);
+  bout <=  borrow(8);
  
-process 
-
-    variable inm   : integer :=0;  
+  process (a)
+    variable inm: integer := 0;  
+    begin
+    
+  --  for i in 7 downto 0 loop
+  --    inm:=to_integer(signed(a));
+  --  end loop;
+  end process;
+  
+scazator_inst:for j in 0 to 7 generate
+scazator_inst: scazator port map (a => a(j), 
+                                  b => b(j), 
+                                  bout => borrow(j+1), 
+                                  bin =>borrow(j), 
+                                  s => s(j));
+  end generate;
+    
+   cp_i(0)<=a(0);
+   cp_i(1)<=a(1);
+   cp_i(2)<=a(2);
+   cp_i(3)<=a(3);
+  out1:  bcd_7segment  port map (cp_i,cp_o);
+  
+   cp_i(0)<=a(4);
+  cp_i(1)<=a(5);
+  cp_i(2)<=a(6);
+  cp_i(3)<=a(7);
+  out2:  bcd_7segment  port map (cp_i,cp_o);
   
   
-  begin
-  
-    for i in 7 downto 0 loop
-  inm<=
-  end loop;
-  
-  
-    for j in 0 to inm loop
-      for i in 0 to 7 loop
-sumator_inst: scazator_complet port map (a(j), b(i), bout(i), bout(i), s(i));
-      end loop;
-    end loop;
- end process;
- 
    
- cp_i(0)<=a(0);
- cp_i(1)<=a(1);
- cp_i(2)<=a(2);
- cp_i(3)<=a(3);
-out1:  bcd_7segment  port map (cp_i,cp_o);
-
- cp_i(0)<=a(4);
-cp_i(1)<=a(5);
-cp_i(2)<=a(6);
-cp_i(3)<=a(7);
-out2:  bcd_7segment  port map (cp_i,cp_o);
-
- cp_i(0)<=a(8);
-cp_i(1)<=a(9);
-cp_i(2)<=a(10);
-cp_i(3)<=a(11);
-out3:  bcd_7segment  port map (cp_i,cp_o);
-
- cp_i(0)<=a(12);
-cp_i(1)<=a(12);
-cp_i(2)<=a(14);
-cp_i(3)<=a(15);
-out4:  bcd_7segment  port map (cp_i,cp_o);
-end Behavioral;
+  end Behavioral;
