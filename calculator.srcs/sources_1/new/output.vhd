3e73a504848f0d8588d9c4e48bdc17ab6dda7d5b
Iulika2901 <84437252+Iulika2901@users.noreply.github.com>
2024-05-13 11:12:35 +0300

    Update output.vh

--- calculator.srcs/sources_1/new/output.vhd
+++ calculator.srcs/sources_1/new/output.vhd
@@ -14,29 +14,23 @@ process(BCDin)
 begin
  
 case BCDin is
-when "0000" =>
-Seven_Segment <= "0000001"; ---0
-when "0001" =>
-Seven_Segment <= "1001111"; ---1
-when "0010" =>
-Seven_Segment <= "0010010"; ---2
-when "0011" =>
-Seven_Segment <= "0000110"; ---3
-when "0100" =>
-Seven_Segment <= "1001100"; ---4
-when "0101" =>
-Seven_Segment <= "0100100"; ---5
-when "0110" =>
-Seven_Segment <= "0100000"; ---6
-when "0111" =>
-Seven_Segment <= "0001111"; ---7
-when "1000" =>
-Seven_Segment <= "0000000"; ---8
-when "1001" =>
-Seven_Segment <= "0000100"; ---9
-when others =>
-Seven_Segment <= "1111111"; ---null
-end case;
+    when "0000" => Seven_Segment <= "0000001"; -- "0"     
+    when "0001" => Seven_Segment <= "1001111"; -- "1" 
+    when "0010" => Seven_Segment <= "0010010"; -- "2" 
+    when "0011" => Seven_Segment <= "0000110"; -- "3" 
+    when "0100" => Seven_Segment <= "1001100"; -- "4" 
+    when "0101" => Seven_Segment <= "0100100"; -- "5" 
+    when "0110" => Seven_Segment <= "0100000"; -- "6" 
+    when "0111" => Seven_Segment <= "0001111"; -- "7" 
+    when "1000" => Seven_Segment <= "0000000"; -- "8"     
+    when "1001" => Seven_Segment <= "0000100"; -- "9" 
+    when "1010" => Seven_Segment <= "0000010"; -- a
+    when "1011" => Seven_Segment <= "1100000"; -- b
+    when "1100" => Seven_Segment <= "0110001"; -- C
+    when "1101" => Seven_Segment <= "1000010"; -- d
+    when "1110" => Seven_Segment <= "0110000"; -- E
+    when "1111" => Seven_Segment <= "0111000"; -- F
+    end case;
  
 end process;
  
