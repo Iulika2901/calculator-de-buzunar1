dd3445d7e94aaa36790210280560bc2964edefe3
Iulika2901 <84437252+Iulika2901@users.noreply.github.com>
2024-05-14 20:59:25 +0300

    Update sumat_complet.vhd

--- calculator.srcs/sources_1/new/sumat_complet.vhd
+++ calculator.srcs/sources_1/new/sumat_complet.vhd
@@ -31,12 +31,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
+
 entity sumat_compl is
-    Port ( a : in STD_LOGIC_vector (7 downto 0);
-           b : in STD_LOGIC_vector (7 downto 0);
+    Port ( a : in STD_LOGIC_vector (6 downto 0);
+           b : in STD_LOGIC_vector (6 downto 0);
            cout : out STD_LOGIC;
            cin : in STD_LOGIC;
-           s : out STD_LOGIC_vector (7 downto 0));
+           s : out STD_LOGIC_vector (11 downto 0));
 end sumat_compl;
 
 architecture Behavioral of sumat_compl is
@@ -56,14 +57,14 @@ Seven_Segment : out STD_LOGIC_VECTOR (6 downto 0));
 end component;
 
   signal cp_i: std_logic_vector(3 downto 0);
-  signal cp_o: std_logic_vector(3 downto 0);
+  signal cp_o: std_logic_vector(6 downto 0);
 
 signal carry: STD_LOGIC_vector (8 downto 0);
 
 begin
 carry(0)<=cin;
 cout<=carry(7);
-eth: for i in 0 to 7 generate
+eth: for i in 0 to 6 generate
 sumator_inst: sumator port map (a => a(i), 
                                 b => b(i), 
                                 cout => carry(i+1), 
@@ -81,7 +82,7 @@ out1:  bcd_7segment  port map (cp_i,cp_o);
  cp_i(0)<=a(4);
 cp_i(1)<=a(5);
 cp_i(2)<=a(6);
-cp_i(3)<=a(7);
+cp_i(3)<='0';
 out2:  bcd_7segment  port map (cp_i,cp_o);
 
 
