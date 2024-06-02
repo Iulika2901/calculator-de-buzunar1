3610a4a28adeb51f25df7f7d1c923e983f361421
Iulika2901 <84437252+Iulika2901@users.noreply.github.com>
2024-06-02 21:19:18 +0300

    Update inmultitor.vhd

--- calculator.srcs/sources_1/new/inmultitor.vhd
+++ calculator.srcs/sources_1/new/inmultitor.vhd
@@ -5,7 +5,8 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity multiplication is
 	port( X: in std_logic_vector (6 downto 0);
 		Y: in std_logic_vector (6 downto 0);
-		CLK,RST: in std_logic;
+		--CLK,
+		RST: in std_logic;
 		rez: out std_logic_vector(11 downto 0);
 		carryM: out std_logic);
 end entity;
@@ -14,13 +15,13 @@ architecture multiplication_a of multiplication is
 begin									
 	process(RST)
 	variable A, B: std_logic_vector(15 downto 0):=(others => '0');
-	variable Q: std_logic_vector(7 downto 0);
+	variable Q: std_logic_vector(6 downto 0);
 	variable N: std_logic_vector(3 downto 0);
 	begin
-if CLK'event and CLK='1' then
+--if CLK'event and CLK='1' then
 		if RST = '1' then
 			Q:=Y;
-			B(7 downto 0) := X;
+			B(6 downto 0) := X;
 			carryM <= '0';
 			A:= x"0000";
 		else
@@ -30,15 +31,15 @@ if CLK'event and CLK='1' then
 				end if;
 				B(15 downto 1) := B(14 downto 0);
 				B(0) := '0';
-				Q(6 downto 0) := Q(7 downto 1);
-				Q(7) := '0';
+				Q(5 downto 0) := Q(6 downto 1);
+				Q(6) := '0';
 				
 				if A > x"FF" then
 					carryM <= '1'; 	 	   
 				end if;
 			end loop;
 		end if;
-			rez <= A(7 downto 0);
-end if;
+			rez <= A(11 downto 0);
+--end if;
 	end process;
 end architecture;
